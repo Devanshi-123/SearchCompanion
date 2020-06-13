a749b34849c20b9116e3972b5c16384cd859027e
Devanshi goel <devanshi@gmail.com>
2020-06-12 18:28:59 -0700

    Updates

---
+++ precache-manifest.383a85428617d4c80ffe0dae488d497b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03be4f2530d840613325abe4139d19f6",
+    "url": "/SearchCompanion/index.html"
+  },
+  {
+    "revision": "984e105051fb5e5f1c66",
+    "url": "/SearchCompanion/static/css/main.761566c2.chunk.css"
+  },
+  {
+    "revision": "a4fb1abb84e339d1f2bd",
+    "url": "/SearchCompanion/static/js/2.d7828487.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SearchCompanion/static/js/2.d7828487.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "984e105051fb5e5f1c66",
+    "url": "/SearchCompanion/static/js/main.f8d73ec9.chunk.js"
+  },
+  {
+    "revision": "5698b270b1dd4863066b",
+    "url": "/SearchCompanion/static/js/runtime-main.931959bb.js"
+  }
+]);
